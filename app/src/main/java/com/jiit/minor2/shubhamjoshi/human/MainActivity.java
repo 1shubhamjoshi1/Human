e4c0346072a6dcf96c78cd8a59dc2437c1b8e541
shubham <1shubhamjoshi1@gmail.com>
2016-09-04 17:59:35 +0530

    Removed searchView

--- app/src/main/java/com/jiit/minor2/shubhamjoshi/human/MainActivity.java
+++ app/src/main/java/com/jiit/minor2/shubhamjoshi/human/MainActivity.java
@@ -14,22 +14,22 @@ import android.view.View;
 
 import com.jiit.minor2.shubhamjoshi.human.Adapters.HomeAdapter;
 import com.jiit.minor2.shubhamjoshi.human.modals.Items;
-import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    MaterialSearchView searchView;
+
     private List<Items> mList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rView);
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        initSearchView();
+
+
         String data1[]={"a","b"};
         String data2[]={"a","b"};
         test();
@@ -47,72 +47,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    private void initSearchView() {
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //Do some magic
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Do some magic
-                return false;
-            }
-        });
-
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                //Do some magic
-            }
-
-            @Override
-            public void onSearchViewClosed() {
-                //Do some magic
-            }
-        });
-
-        searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
-        searchView.setCursorDrawable(R.drawable.custom_search_cursor);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
-
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
-            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            if (matches != null && matches.size() > 0) {
-                String searchWrd = matches.get(0);
-                if (!TextUtils.isEmpty(searchWrd)) {
-                    searchView.setQuery(searchWrd, false);
-                }
-            }
-
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (searchView.isSearchOpen()) {
-            searchView.closeSearch();
-        } else {
-            super.onBackPressed();
-        }
-    }
 
 
     void test()
