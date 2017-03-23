d63868528deac850d0ea880ceaa996215f7c1f48
shubham <1shubhamjoshi1@gmail.com>
2017-03-24 03:47:30 +0530

    Timestamp added in recent activity

--- app/src/main/java/com/jiit/minor2/shubhamjoshi/human/App/IntroPage.java
+++ app/src/main/java/com/jiit/minor2/shubhamjoshi/human/App/IntroPage.java
@@ -59,6 +59,7 @@ public class IntroPage extends AppCompatActivity {
             @Override
             public void populateViewHolder(PostHolder chatMessageViewHolder, Post chatMessage, int position) {
                 Picasso.with(getBaseContext()).load(chatMessage.getImageUrl()).into(chatMessageViewHolder.mNameField);
+                chatMessageViewHolder.setDate(chatMessage.getTimestamp()*-1);
                 chatMessageViewHolder.setText(chatMessage.getMatter());
 
             }
@@ -69,17 +70,59 @@ public class IntroPage extends AppCompatActivity {
     public static class PostHolder extends RecyclerView.ViewHolder {
         private final CircleImageView mNameField;
         private final TextView mTextField;
+        private final TextView mdate;
 
         public PostHolder(View itemView) {
             super(itemView);
             mNameField = (CircleImageView) itemView.findViewById(R.id.img);
             mTextField = (TextView) itemView.findViewById(R.id.text);
+            mdate = (TextView)itemView.findViewById(R.id.date);
         }
 
-
+        public void setDate(long l)
+        {
+            mdate.setText(getTimeAgo(l));
+        }
 
         public void setText(String text) {
             mTextField.setText(text);
+        }
+    }
+
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        // TODO: localize
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "1m ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + "m ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "1h ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + "h ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + "d ago";
         }
     }
 }
